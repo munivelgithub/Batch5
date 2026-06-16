34004b1d2e65d9632d7e65dad91ca2287a32e850
Jeejo <jeejovetharaj@gmail.com>
2026-06-16 23:09:47 +0530

    good day

--- as.c
+++ as.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!");
+    printf("Hello, World!")bk kjb kkkkb jk jk jk jk h hj k  
+    iojnoj jio jinnjn;
     return 0;
 }
 kl klj jkl kkk
